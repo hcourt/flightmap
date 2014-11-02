d2f2c4eb732675bfbddf1b6d3e2ac9e542d1228e
Hazel Court <hcourt@uchicago.edu>
2014-11-02 14:12:31 -0600

    can now read from flights.txt and make a graph

---
+++ src/graph.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Graph:
+	def __init__(self):
+		self.nodes = set()
+		self.edges = defaultdict(list)
+		self.weights = {}
+
+	def add_node(self, name):
+		self.nodes.add(name)
+
+	def add_edge(self, start, end, weight):
+		self.edges[start].append(end)
+		self.edges[end].append(start)
+		self.weights[(start, end)] = weight
--- src/main.py
+++ src/main.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python
 
 import sys, getopt
+from graph import *
+
+def build_map (input):
+	print 'Building map...'
+
+	map = Graph()
+
+	for line in input.readlines():
+
+		args = line.rstrip().split(',', 2)
+		if args[2].isdigit():
+			map.add_node(args[0])
+			map.add_node(args[1])
+			map.add_edge(args[0], args[1], args[2])
+		else:
+			print 'Error: received improper map input.\n'
+			sys.exit(1)
+
+	print 'Build successful.\n'
+	
+def do_tasks (input, output, map):
+	print 'Executing tasks...'
+
+
+
+	print 'Execution successful.\n'
 
 def main (argc, argv):
 	flightfile = ''
+	taskfile = ''
    	outputfile = ''
    	try:
 		opts, args = getopt.getopt(argv,'hf:t:o:',['flights=','tasks=', 'out='])
 	except getopt.GetoptError:
 		print 'usage: ./main.py -f <flightfile> -t <taskfile> -o <outputfile>'
-      	#sys.exit(2)
   	else:
   		if opts != []:
 			for opt, arg in opts:
@@ -22,12 +48,23 @@ def main (argc, argv):
 					taskfile = arg
 				elif opt in ('-o', '--out'):
 					outputfile = arg
+			print ''
 			print 'Flight file is: ' + flightfile
 			print 'Task file is: ' + taskfile
 			print 'Output file is: ' + outputfile
+			print ''
 
 			output = open(outputfile, 'w')
-			output.write ('hello there\n')
+			flights = open(flightfile, 'r')
+			tasks = open(taskfile, 'r')
+
+			map = build_map(flights)
+			do_tasks(tasks, output, map)
+
+			output.close()
+			flights.close()
+			tasks.close()
+
 
 if __name__ == '__main__':
 	main(len(sys.argv[1:]), sys.argv[1:])
